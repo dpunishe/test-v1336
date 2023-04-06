e5614c23c2240665d71da637959550de30cc114b
dpunishe <dpunishe@yandex.ru>
2023-04-06 13:55:05 -0400

    uWu

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from '../App';
 import {Provider} from 'react-redux';
-import {store} from './store';
+import {store} from '../store';
 
 const root = document.getElementById('root');
 
 const render = () => {
   ReactDOM.createRoot(root).render(
-    <React.StrictMode>
       <Provider store={store}>
         <App />
       </Provider>
-    </React.StrictMode>
   );
 };
 
