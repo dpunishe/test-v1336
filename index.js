36b6d87375cc8f70010a9425e2391841248d1655
dpunishe <dpunishe@yandex.ru>
2023-04-06 11:21:54 -0400

    deploy3

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './src/App';
 import {Provider} from 'react-redux';
-import {store} from './store';
+import {store} from './src/store';
 
 const root = document.getElementById('root');
 
