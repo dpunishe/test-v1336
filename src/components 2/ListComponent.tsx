9cae196dc07d1ae8c286891e464ad0b8fc7acef8
dpunishe <dpunishe@yandex.ru>
2023-04-06 18:45:44 -0400

    vse

--- src/components 2/ListComponent.tsx
+++ src/components 2/ListComponent.tsx
@@ -1,9 +1,10 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { Card, List, Select, Spin } from 'antd';
 import { setListDataDefault, setListDataUi } from '../reducers/reducer';
 import { getBrigades } from '../axios/api';
 import './ListComponent.css';
+import { useSelector } from 'react-redux';
 
 const { Option } = Select;
 
@@ -26,14 +27,12 @@ export interface Data {
 }
 
 interface RootState {
-  brigades: { listDataUi: Data[]; listDataDefault: Data[] };
+  brigades: { listDataUi: Data[]; listDataDefault: Data[]; }
 }
 
 const ListComponent: React.FC = () => {
   const dispatch = useDispatch();
-  const { listDataUi, listDataDefault } = useSelector(
-    (state: RootState) => state.brigades
-  );
+  const { listDataUi, listDataDefault } = useSelector((state: RootState) => state.brigades)
   const [filterByDepID, setFilterByDepID] = useState<string>('');
   const [filterByConnection, setFilterByConnection] = useState<string>('');
   const [loading, setLoading] = useState<boolean>(false);
@@ -51,19 +50,12 @@ const ListComponent: React.FC = () => {
   }, [fetchData]);
 
   useEffect(() => {
-    const filteredData = listDataDefault.filter((item) => {
-      if (filterByConnection && filterByDepID) {
-        return (
-          item.department.id === Number(filterByDepID) &&
-          item.connectionStateId === Number(filterByConnection)
-        );
-      }
-      if (filterByConnection) {
-        return item.connectionStateId === Number(filterByConnection);
-      }
-      return item.department.id === Number(filterByDepID);
-    });
-    dispatch(setListDataUi(filteredData));
+    if (filterByConnection && filterByDepID) {
+      dispatch(setListDataUi(listDataDefault.filter(item => item.department.id === Number(filterByDepID) && item.connectionStateId === Number(filterByConnection))));
+      return;
+    }
+    if (filterByConnection) dispatch(setListDataUi(listDataDefault.filter(item => item.connectionStateId === Number(filterByConnection))));
+    else dispatch(setListDataUi(listDataDefault.filter(item => item.department.id === Number(filterByDepID))));
   }, [dispatch, filterByConnection, filterByDepID, listDataDefault]);
 
   const handleDepIDChange = (value: string | null) => {
@@ -74,31 +66,17 @@ const ListComponent: React.FC = () => {
     setFilterByConnection(value ?? '');
   };
 
-  const mobileGrid = { column: 2 };
-  const desktopGrid = { column: 4 };
-  const finalGrid = window.matchMedia('(max-width: 479px)').matches
-    ? mobileGrid
-    : desktopGrid;
-   
   return (
     <Spin spinning={loading}>
       <div>
         <div className="list-component filters">
           <p>Соединение:</p>
-          <Select
-            className="select"
-            value={filterByConnection}
-            onChange={handleConnectionChange}
-          >
+          <Select className='select' value={filterByConnection} onChange={handleConnectionChange}>
             <Option value={'1'}>В норме</Option>
             <Option value={'0'}>Нет связи</Option>
           </Select>
           <p>Департамент:</p>
-          <Select
-            className="select"
-            value={filterByDepID}
-            onChange={handleDepIDChange}
-          >
+          <Select className='select' value={filterByDepID} onChange={handleDepIDChange}>
             <Option value={'0'}>Лукойл </Option>
             <Option value={'1'}>Роснефть </Option>
             <Option value={'2'}>Газпром нефть </Option>
@@ -106,32 +84,25 @@ const ListComponent: React.FC = () => {
         </div>
         <List
           className="list-component"
-          dataSource={listDataUi}
           bordered
-          grid={finalGrid}
-          renderItem={({ connectionStateId, department, brigade_name, position }) => (
-            <List.Item className="list">
+          dataSource={listDataUi}
+          grid={{ gutter: 16, column: 3 }}
+          renderItem={({connectionStateId, department, brigade_name, position }) => (
+            <List.Item>
               <Card
-                className="card"
+                className='card'
                 title={brigade_name}
-                extra={
-                  <div
-                    className={
-                      connectionStateId === 1 ? 'connection on' : 'connection off'
-                    }
-                  ></div>
-                }
+                extra={<div className={(connectionStateId === 1) ? 'connection on' : 'connection off'}></div>}
                 size="small"
               >
-                <h4>
-                  {department.id === 0 ? 'Лукойл' : null}
-                  {department.id === 1 ? 'Роснефть' : null}
-                  {department.id === 2 ? 'Газпром нефть' : null}
+                <h4>{(department.id === 0) ? 'Лукойл' : null}
+                  {(department.id === 1) ? 'Роснефть' : null}
+                  {(department.id === 2) ? 'Газпром нефть' : null}
                 </h4>
-                <p><b>Состояние соединения:</b> {connectionStateId === 1 ? 'В норме' : 'Нет связи'}</p>
-                <p><b>Кластер</b>: {position.cluster}</p>
-                <p><b>Поле</b>: {position.field}</p>
-                <p><b>Скважина</b>: {position.well}</p>
+                <p>Состояние соединения: {(connectionStateId === 1) ? 'В норме' : 'Нет связи'}</p>
+                <p>Кластер: {position.cluster}</p>
+                <p>Поле: {position.field}</p>
+                <p>Скважина: {position.well}</p>
               </Card>
             </List.Item>
           )}
@@ -139,6 +110,6 @@ const ListComponent: React.FC = () => {
       </div>
     </Spin>
   );
-};
+}
 
 export default ListComponent;
